209bcfcd7c0b01e1dbdcba3e8c72d0cc584ffffb
Jan Truchlý <truchjan@fit.cvut.cz>
2024-01-25 15:53:58 +0100

    deploy fe

--- mern02_tennis_courts/backend/src/config/allowedOrigins.js
+++ mern02_tennis_courts/backend/src/config/allowedOrigins.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 
-// TODO - change to real render static site
 const allowedOrigins = [
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : 'https://tennis-static-site.onrender.com'
+  process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : 'https://tennis-courts.onrender.com'
 ]
 
 module.exports = allowedOrigins
--- mern02_tennis_courts/frontend/src/main.tsx
+++ mern02_tennis_courts/frontend/src/main.tsx
@@ -2,6 +2,9 @@ import * as ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { AuthContextProvider } from './context/AuthContext'
+import { disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AuthContextProvider>
